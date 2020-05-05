6953deb7f85f0848483e0ce79487392aa6af514c
Alice105973 <kukushkinako13081999@gmail.com>
2020-05-05 13:07:07 +0300

    lint

--- modules/intersecting-segments/include/segments_app.h
+++ modules/intersecting-segments/include/segments_app.h
@@ -7,12 +7,12 @@
 
 class Segment_app {
  public:
-   Segment_app();
+  Segment_app();
 
  private:
-   void help(const char* appname, const char* message = "");
-   std::string message_;
-   bool validateNumberOfArguments(int argc, const char** argv);
+  void help(const char* appname, const char* message = "");
+  std::string message_;
+  bool validateNumberOfArguments(int argc, const char** argv);
 };
 
 #endif  // MODULES_INTERSECTING_SEGMENTS_INCLUDE_SEGMENTS_APP_H_
